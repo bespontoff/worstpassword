66e5bd44d2f90d4f3cc137d6ef457158a0a63221
bespontoff <bespontoff@gmail.com>
2023-10-27 18:38:04 +0300

    minor refactoring and readme update

--- README.md
+++ README.md
@@ -1,16 +1,27 @@
 # WorstPassword
 
-## Generating a worst passwords.
+## Generating the worst passwords.
+
+This utilite generates passwords from very annoing symbols.
 
 ##### Installation:
 
 `pip install worstpassword`
 
 ##### Usage:
+You can use this utilite in console:
 
-`worstpassword`
+`$ worstpassword`
+
+with length parameter:
+
+`$ worstpassword -l 12`
+
+or
+
+`$ worstpassword --length 8`
 
 ###### options:
 
   `-l LENGTH, --length LENGTH`
-                        Length of password. **Default: 10**
+Length of password. Min length is 4. Max length is 64. **Default length: 10**
--- worstpassword.py
+++ worstpassword.py
@@ -3,16 +3,20 @@ import random
 
 
 class WorstPassword:
-    __characters__ = ('0', 'O', 'l', '1', 'I', '|', '5', 'S')
+    __characters = ('0', 'O', 'l', '1', 'I', '|', '5', 'S')
+    __min_length = 4
+    __max_length = 64
 
-    def __init__(self, length=8):
-        self.length = length
-        self.password = []
+    def __init__(self, length: int = 10):
+        if length < WorstPassword.__min_length:
+            self._length = WorstPassword.__min_length
+        elif length > WorstPassword.__max_length:
+            self._length = WorstPassword.__max_length
+        else:
+            self._length = length
 
-    def generate(self):
-        for char in range(self.length):
-            self.password.append(random.choice(WorstPassword.__characters__))
-        return ''.join(self.password)
+    def generate(self) -> str:
+        return ''.join(random.choices(WorstPassword.__characters, k=self._length))
 
 
 def main():
